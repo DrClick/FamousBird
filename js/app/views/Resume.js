c337d27ab023fa00e87c8a05fead3d6c43113196
Thomas Watson <drclick@mac.com>
2014-02-28 08:13:33 -0800

    Starting Resume

---
+++ js/app/views/Resume.js
@@ -0,0 +1,17 @@
+define(function(require, exports, module) {
+    var Surface         = require('famous/Surface');
+    var Modifier        = require('famous/Modifier');
+    var Matrix          = require('famous/Matrix');
+    var View            = require('famous/View');
+
+    function Resume() {
+        View.apply(this, arguments);
+    }
+
+    Resume.prototype = Object.create(View.prototype);
+    Resume.prototype.constructor = MyView;
+
+    Resume.DEFAULT_OPTIONS = {};
+
+    module.exports = Resume;
+});
