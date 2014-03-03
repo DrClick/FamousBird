d3c613db79223e274c62136be0ca5201dfe34d0e
Thomas Watson <drclick@mac.com>
2014-03-02 20:38:49 -0800

    Controls are working fully

--- js/app/widgets/SlideShow.js
+++ js/app/widgets/SlideShow.js
@@ -7,6 +7,7 @@ define(function(require, exports, module) {
     var View            = require("famous/View");
     var AppUtils        = require("app/Util");
     var Timer           = require("famous-utils/Time");
+    var SpringButton    = require("famous-ui/buttons/SpringButton");
 
     function SlideShow() {
         View.apply(this, arguments);
@@ -21,7 +22,7 @@ define(function(require, exports, module) {
         loop: false,
         autostart: true,
         startDelay: 1000,
-        controlsYOffset: 300,
+        controlsYOffset: 400,
         controlOpacity: .3
     };
 
@@ -38,92 +39,148 @@ define(function(require, exports, module) {
         this.playing = false;
 
         this.controls = {
-            restart: _createControl.call(this, {xPos: -280, control: "restart"}),
-            stop: _createControl.call(this, {xPos: -150, control: "stop"}),
-            backward: _createControl.call(this, {xPos: 0, control: "backward"}),
-            play: _createControl.call(this, {xPos: 150, control: "play"}),
-            forward: _createControl.call(this, {xPos: 280, control: "forward"})
+            restart: _createControl.call(this, {xPos: -260, control: "restart"}),
+            stop: _createControl.call(this, {xPos: -130, control: "stop"}),
+            play: _createControl.call(this, {xPos: 20, control: "pause"}),
+            backward: _createControl.call(this, {xPos: 170, control: "backward"}),
+            forward: _createControl.call(this, {xPos: 300, control: "forward"})
         };
 
+        this.controls.restart.on("click", this.restart.bind(this));
+        this.controls.stop.on("click", this.stop.bind(this));
+        this.controls.play.on("click", this.playPause.bind(this));
+        this.controls.backward.on("click", this.back.bind(this));
+        this.controls.forward.on("click", this.forward.bind(this));
 
        
     }//end create
 
     function _createControl(opts){
 
-        var modifier = new Modifier({
-                transform: Matrix.multiply(
-                    Matrix.translate(opts.xPos,this.options.controlsYOffset,1),
-                    Matrix.scale(1, 1, 0)),
-                opacity: this.options.controlOpacity,
-                origin: [.5, .5]
+        var button = new SpringButton({
+            content: "<div class='icon-slide-" + opts.control + "'></div>",
+            size: [80,80],
+            pos: [opts.xPos, this.options.controlsYOffset, 1]
+
         });
 
-        //add the cloud off screen
-        var surface = new Surface({
-            size : [80, 80],
-            classes : ["controls", "icon-slide-" + opts.control]
-        });
+        this._add(new Modifier({origin:[.5,.5]})).link(button);
 
-        this._add(modifier).link(surface);
-
-        return surface;
+        return button;
     }//end create controls
 
+    SlideShow.prototype.restart = function(){
+        this.isPlaying = true;
+        this.currentSlide = 0;
+        _showSlide.call(this, this.currentSlide); 
+    };//end start
+    
 
     SlideShow.prototype.start = function(){
         this.isPlaying = true;
+        this.controls.play.surface.setContent("");
+        this.controls.play.surface.setClasses(['icon-slide-pause']);
         _showSlide.call(this, this.currentSlide); 
     };//end start
 
-    SlideShow.prototype.pause = function(){
+    SlideShow.prototype.stop = function(){
         this.isPlaying = false;
+        this.controls.play.surface.setClasses(['icon-slide-play']);
+    };//end start
+
+    SlideShow.prototype.playPause = function(){
+        if(this.isPlaying){
+            this.stop();
+        } else{
+            this.start();
+        }
     };//end start
 
     SlideShow.prototype.resume = function(){
         this.isPlaying = true;
     };//end start
 
+    SlideShow.prototype.forward = function(){
+        var slideNum = (this.currentSlide + 1) % this.slides.length;
+        this.currentSlide = slideNum;
+        _showSlide.call(this, slideNum);
+    };//end start
+
+    SlideShow.prototype.back = function(){
+        var slideNum = (this.currentSlide - 1) % this.slides.length;
+        this.currentSlide = slideNum;
+        _showSlide.call(this, slideNum);
+    };//end start
+
+
     SlideShow.prototype.skipTo = function(slideNumber){
-        _show.call(this, slideNumber);
+        _showSlide.call(this, slideNumber);
     };//end start
 
 
     function _showSlide(slideNumber){
         var slide = this.slides[slideNumber];
 
-        AppUtils.loadFragment("/fragments/" + slide.href, {}, function(data){
-            var surface = new Surface({
-                classes: ['slide', 'slide_' + slideNumber],
-                size: this.options.size,
-                content: data
-            });
-            var modifier = new Modifier({
-                transform: Matrix.translate(0,0,0),
-                origin: this.options.origin,
-                opacity: 0
-            });
+        //hide any slide accept the requsted
+        for (var i = 0; i < this.slides.length; i++) {
+            if(i != slideNumber && this.slides[i].obj){
+                this.slides[i].obj.modifier.setOpacity(0, {duration:100});
+            }
+        };
+
+        if(!slide.obj){
+            AppUtils.loadFragment("/fragments/" + slide.href, {}, function(data){
+                var surface = new Surface({
+                    classes: ['slide', 'slide_' + slideNumber],
+                    size: this.options.size,
+                    content: data
+                });
+                var modifier = new Modifier({
+                    transform: Matrix.translate(0,0,0),
+                    origin: this.options.origin,
+                    opacity: 0
+                });
 
 
-            surface.pipe(this.eventOutput);
+                surface.pipe(this.eventOutput);
 
-            //add it to the view
-            this._add(modifier).link(surface);
+                //add it to the view
+                slide.obj = {surface: surface, modifier: modifier};
+                this._add(modifier).link(surface);
 
-            //animate it
-            modifier.setOpacity(1, {duration:600});
-
-            if(this.isPlaying && this.currentSlide < (this.slides.length - 1)){
-                Timer.setTimeout(function(){
-                    modifier.setOpacity(0, {duration:300});
-                    this.currentSlide++;
-                    _showSlide.call(this, this.currentSlide);
-                }.bind(this), slide.duration);
-            }//end if playing
-
-        }.bind(this));
+                _animateSlide.call(this, slideNumber);
+            }.bind(this));
+        }//end if slide not loaded
+        else{
+            _animateSlide.call(this, slideNumber);
+        }//end if slide loaded
     }//end function
 
+    function _animateSlide(slideNumber){
+         //animate it
+            var slide = this.slides[slideNumber];
+            var modifier = slide.obj.modifier;
+            modifier.setOpacity(1, {duration:600});
+
+            //animate out if not the last slide or if looping
+            var showNext = this.isPlaying && this.currentSlide < (this.slides.length - 1);
+            showNext = showNext || 
+                (this.currentSlide == (this.slides.length - 1) && this.options.loop);
+
+            if(showNext){
+                if(this.animationTimer){Timer.removeTimeout(this.animationTimer);}
+                this.animationTimer = Timer.setTimeout(function(){_showNext.call(this, slideNumber)}.bind(this), slide.duration);
+            }//end if playing
+        
+    }//end animateSlid
+
+
+    function _showNext(slideNumber){
+        if(this.isPlaying){
+            this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+            _showSlide.call(this, this.currentSlide);
+        }//end if playing
+    }//end amimate out
 
     module.exports = SlideShow;
 });
