36fb540c46403a176b5b4c68296b8b36607b44ad
drclick <drclick@mac.com>
2014-03-24 17:08:24 -0700

    Engine onResize

--- js/app.js
+++ js/app.js
@@ -5,30 +5,39 @@ define(function(require, exports, module) {
         var Transform = require("famous/Transform");
         var GameView        = require("app/views/Game");
 
-         //scale the window
-        var scaleY = window.innerHeight / 960;
-        var scaleX = window.innerWidth / 640;
-        var scale = Math.min(scaleX, scaleY);
-
-        console.log(scale);
-
-        var appWidth = 640 * scale;
-        var appHeight = 960 * scale;
-
         var contextContainer = document.getElementById("contextContainer");
-        contextContainer.style.width = appWidth + "px";
-        contextContainer.style.height = appHeight + "px";
+        contextContainer.style.width = "640px";
+        contextContainer.style.height = "960px";
 
         //create the new one
         var context = Engine.createContext(contextContainer);
-
+        var modifier = new Modifier({
+            origin: [.5,.5],
+            size: [640, 960]
+        });
 
         var game = new GameView();
 
-        context.add(new Modifier({
-            origin: [.5,.5],
-            size: [640, 960],
-            transform: Transform.scale(scale, scale, 1)
-        })).add(game);
+        context.add(modifier).add(game);
+
+        _resize();
+        Engine.on("resize", _resize);
+
+
+        function _resize(){
+             //scale the window
+            var scaleY = window.innerHeight / 960;
+            var scaleX = window.innerWidth / 640;
+            var scale = Math.min(scaleX, scaleY);
+
+            var appWidth = 640 * scale;
+            var appHeight = 960 * scale;
+
+            var contextContainer = document.getElementById("contextContainer");
+            contextContainer.style.width = appWidth + "px";
+            contextContainer.style.height = appHeight + "px";
+
+            modifier.setTransform(Transform.scale(scale, scale, 1));
+        }
 });
 
